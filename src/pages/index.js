4982a5f988f2d73c8c51dbcad7a22afd3814a099
Perseu <71997803+perseu444@users.noreply.github.com>
2023-07-15 20:54:17 -0300

    Container and subtitles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,32 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Navbar from '@/components/navbar/navbar'
+import Navbar from "@/components/navbar/navbar";
+import Subtitle from "@/components/tipography/subtitle/subtitle";
+import Container from '@/components/container/container'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>DevSteam: A sua loja online de games</title>
-        <meta name="description" content="DevSteam: A sua loja online de games" />
+        <meta
+          name="description"
+          content="DevSteam: A sua loja online de games"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
         <Navbar />
+        <Container>
+          <div>
+            <Subtitle>Promoções</Subtitle>
+          </div>
+          <div>
+            <Subtitle>Outros jogos</Subtitle>
+          </div>
+        </Container>
       </div>
     </>
-  )
+  );
 }
