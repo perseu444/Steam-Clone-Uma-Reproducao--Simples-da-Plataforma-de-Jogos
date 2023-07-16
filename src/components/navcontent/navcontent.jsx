82c9c6bdebb7088d2cf402fa9ddf382b5119a462
Perseu <71997803+perseu444@users.noreply.github.com>
2023-07-16 20:51:30 -0300

    Reponsive added

--- src/components/navcontent/navcontent.jsx
+++ src/components/navcontent/navcontent.jsx
@@ -3,14 +3,16 @@ import styles from './navcontent.module.css'
 export default function Navcontent() {
     return (
         <div className={styles.navcontent}>
-            <ul>
-                <li><a href="#" className={styles.checked}>LOJA</a></li>
-                <li><a href="#">COMUNIDADE</a></li>
-                <li><a href="#">USER</a></li>
-                <li><a href="#">CONVERSA</a></li>
-                <li><a href="#">SUPORTE</a></li>
+            <div className={styles.navtext}>
+                <ul >
+                    <li><a href="#" className={styles.checked}>LOJA</a></li>
+                    <li><a href="#">COMUNIDADE</a></li>
+                    <li><a href="#">USER</a></li>
+                    <li><a href="#">CONVERSA</a></li>
+                    <li><a href="#">SUPORTE</a></li>
+                </ul>
+            </div>
 
-            </ul>
 
         </div>
     )
--- src/components/usericon/usericon.jsx
+++ src/components/usericon/usericon.jsx
@@ -3,33 +3,33 @@ import style from './usericon.module.css'
 import { BsBellFill } from 'react-icons/bs'
 import { AiFillCaretDown } from 'react-icons/ai'
 
-export default function Usericon(){
-    return(
+export default function Usericon() {
+    return (
         <div className={style.usericon}>
             <div className={style.botao1}>
-                <Image src='/downloadsteam.png' width={10} height={10}/>
-                    <p>
-                        Instale o Steam
-                    </p>            
+                <Image src='/downloadsteam.png' width={10} height={10} />
+                <p>
+                    Instale o Steam
+                </p>
             </div>
             <div className={style.botao2}>
-                <BsBellFill size={13}/>
-                
+                <BsBellFill size={13} />
+
             </div>
             <div className={style.userlabel}>
                 <div className={style.username}>
-                <p>Persheus</p>
-                <AiFillCaretDown size={10} />
+                    <p>Persheus</p>
+                    <AiFillCaretDown size={10} />
                 </div>
-            <div className={style.usercoin}>
-            <p>R$ 0.00</p>
+                <div className={style.usercoin}>
+                    <p>R$ 0.00</p>
+
+                </div>
+
 
             </div>
-            
 
-            </div>
-            
-            <Image src='/usericon.png' width={40} height={40}/>
+            <Image className={style.userpfp} src='/usericon.png' width={40} height={40} />
         </div>
     )
 }
